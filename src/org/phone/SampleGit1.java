86ae4e3bbda9729a9a63cf4f0b1e33086b41c4c9
VelammalRevathy <revathyrva@gmail.com>
2021-08-03 17:03:49 +0530

    Password

---
+++ src/org/phone/SampleGit1.java
@@ -0,0 +1,9 @@
+package org.phone;
+public class SampleGit1 {
+	public static void password() {
+		System.out.println("Password");
+		}
+public static void main(String[] args) {
+	password();
+	}
+}
