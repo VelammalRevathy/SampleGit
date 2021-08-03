3713102a45426c7192a6437ca98c2231bf48940f
VelammalRevathy <revathyrva@gmail.com>
2021-08-03 16:56:11 +0530

    UserName

---
+++ src/org/phone/SampleGit1.java
@@ -0,0 +1,9 @@
+package org.phone;
+public class SampleGit1 {
+	public static void userName() {
+		System.out.println("UserName");
+}
+  public static void main(String[] args) {
+	userName();
+  }	
+}
